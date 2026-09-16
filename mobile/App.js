31877d57118ef5b9ab878f3f926e47389d5199aa
ilse547 <lilieco@pm.me>
2026-09-16 13:50:47 +0200

    change bottom bar colour

--- mobile/App.js
+++ mobile/App.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
   bottomBar: {
     width: "100%",
     height: 80,
-    backgroundColor: "#ffffff",
+    backgroundColor: "#ccffcc",
     alignSelf: "stretch",
     alignItems: "center",
     justifyContent: "center"
