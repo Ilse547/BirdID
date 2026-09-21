cfb4d69327f510f18ec151629eed8ba346731fab
ilse547 <lilieco@pm.me>
2026-09-21 10:57:08 +0200

    create recording page

---
+++ mobile/pages/Recordingpage.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import styles from "../styles";
+
+export default function Recordinpage() {
+	return(
+		<View style={styles.mainContent}>
+			<Text>Recording</Text>
+		</View>
+	);
+}
