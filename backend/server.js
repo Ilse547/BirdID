4aa57fa5d21e76525249bd735c856f6ef7f26c96
ilse547 <lilieco@pm.me>
2026-09-24 15:47:17 +0200

    add upload route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+const uploadDirectory = path.join(__dirname, 'upload');
+if(!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
+const upload = multer({ dest: uploadDirectory});
+
 app.get("/", (req, res) => {
   res.json({
     message: "Hello from the backend"
+  });
+});
+
+app.post("/upload", upload.single("audio"), (req, res) => {
+  if(!req.file) {
+    return res.status(400).json({
+      error: "No audio file uploaded"
+    });
+  }
+  res.json({
+    message: "audio uploadedd",
+    filename: req.file.filename,
+    path: req.file.path,
+    originalName: req.file.originalname
   });
 });
 
